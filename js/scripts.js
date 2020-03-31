7a05a1b1ca818def7acc9b226c5a578b9d76714b
Matt Stroud <lixingke06@gmail.com>
2020-03-31 14:40:39 -0700

    make leapYear return true if year % 400 === 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function leapYear(year) {
-  if ((year % 100 !== 0) && (year % 4 === 0)) {
+  if ((year % 100 !== 0) && (year % 4 === 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
