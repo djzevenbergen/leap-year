be90d52877f4c54fcff3bb4039f2e147e5dddd19
Matt Stroud <lixingke06@gmail.com>
2020-03-31 14:25:08 -0700

    create leapYear function, and event listener for submit button to call it

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
+function leapYear(){
 
+  
+}
+
+
+
+$(document).ready(function() {
+  $("#textForm").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
 });
